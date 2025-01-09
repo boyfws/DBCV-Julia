7504b12b0f53961ffef0f3484e531f90d02ffa35
boyfws <minkin.d.d@gmail.com>
2025-01-09 19:52:56 +0300

    Промежуточный фикс

--- src/calcDBCV.jl
+++ src/calcDBCV.jl
@@ -1,6 +1,9 @@
 include("calcNumOccur.jl")
+include("find_Dsbcs_IntOb_IntCoreDist.jl")
+include("findIndex.jl")
 
 using LinearAlgebra
+using Base.Threads
 
 function calcDBCV(
     matrix::Symmetric{Float64}, 
@@ -25,11 +28,33 @@ function calcDBCV(
     # Инициализируем переменные 
     dsbcs::Vector{Float64} = fill(Float64(0), uniq_val_len)
 
-    min_dspcs::Vector{Atomic{Float64}} = fill(Atomic{Float64}(Inf), uniq_val_len)
+    min_dspcs::Vector{Float64} = fill(Float64(Inf), uniq_val_len)
 
     internal_objects_per_cls = Dict{Int64, Vector{Int64}}()
 
-    internal_core_dists_per_cls = Dict{Int64, Vector{Float64}}()
+    internal_core_dists = Dict{Int64, Vector{Float64}}()
+
+
+    for i in 1:uniq_val_len
+        index::Vector{Int64} = findIndex(labels, uniq_val_len[i])
+
+        dsbcs[i], internal_objects_per_cls[i], internal_core_dists[i] = find_Dsbcs_IntOb_IntCoreDist(matrix, index, d)
+
+    end 
+
+
+    for i in 1:uniq_val_len
+
+        for j in i+1:uniq_val_len
+
+
+        end
+
+    end
+
+
+
+    
 
 
 
---
+++ src/findIndex.jl
@@ -0,0 +1,24 @@
+# Нужно провести ресерч для  опредления границы
+const BOUND_FOR_PAR_EXEC = 100
+
+function findIndex(labels::Vector{Int64}, el::Int64)::Vector{Int64}
+    """
+    Функция для нахождения индексов вхождений оопредленного элемента 
+    """
+    n = length(labels)
+    if n < BOUND_FOR_PAR_EXEC
+        return Vector{Int64}([j for j in 1:n if labels[j] == el])
+    end 
+
+    index = Vector{Int64}()
+    lock = ReentrantLock()  # Для синхронизации доступа к массиву
+
+    @threads for j in 1:n
+        if labels[j] == el
+            lock(lock) do
+                push!(index, j)
+            end
+        end
+    end
+    return index
+end
